57917ea30cee126f6e2dc7ff4dbb0216e3b37fe9
Juan Fernando Restrepo <JuanRestrepo>
2020-07-22 19:19:56 -0500

    Usando directivas ngIf y ngFor

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+interface Frase {
+    mensaje: string;
+    autor: string;
+}
 
 @Component({
     selector: 'app-body',
     templateUrl: './body.component.html'
 })
 export class BodyComponent {
+
+    frase: Frase = {
+        mensaje: 'Un gran poder requiere una gran responsabilidad',
+        autor: 'Ben Parker'
+    };
+
+    mostrar = false;
+    personajes: string[] = ['Spiderman', 'Venom', 'Dr Octopus'];
+
+    mostrarOcultarFrase = () => {
+        this.mostrar = !this.mostrar;
+    }
+
+    
 
 }
